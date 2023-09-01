14c9088cf8fb0c6854287dbb4edaa3ee68a6a58e
Ramkumar Natarajan <nrkumar93@gmail.com>
2023-09-01 16:39:39 -0400

    playing with maze

---
+++ reproduction/maze_example.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from random import choice, randint, seed
+
+from pydrake.geometry.optimization import HPolyhedron
+from pydrake.solvers import MosekSolver
+
+from gcs.bezier import BezierGCS
+from gcs.linear import LinearGCS
+from models.maze import Maze
+
+os.environ["MOSEKLM_LICENSE_FILE"] = "/home/gaussian/Documents/softwares/mosektoolslinux64x86/mosek.lic"
+MosekSolver.AcquireLicense()
+print("Mosek is enabled: ", MosekSolver().enabled())
+
+seed(4)
+
+maze_size = 50
+knock_downs = 100
+
+start = np.array([0.5, 0])
+goal = np.array([maze_size - 0.5, maze_size])
+maze = Maze(maze_size, maze_size)
+maze.make_maze()
+
+while knock_downs > 0:
+    cell = maze.cell_at(randint(1, maze_size - 2), randint(1, maze_size - 2))
+    walls = [w for w, up in cell.walls.items() if up]
+    if len(walls) > 0:
+        maze.knock_down_wall(cell, choice(walls))
+        knock_downs -= 1
+
+regions = []
+edges = []
+for x in range(maze_size):
+    for y in range(maze_size):
+        regions.append(HPolyhedron.MakeBox([x, y], [x+1., y+1.]))
+        C = y + x * maze.ny
+        if not maze.map[x][y].walls['N']:
+            edges.append((C, C + 1))
+        if not maze.map[x][y].walls['S']:
+            edges.append((C, C - 1))
+        if not maze.map[x][y].walls['E']:
+            edges.append((C, C + maze.ny))
+        if not maze.map[x][y].walls['W']:
+            edges.append((C, C - maze.ny))
+
+def plot_maze():
+    plt.figure(figsize=(5,5))
+    plt.axis('equal')
+    maze.plot(1)
+    plt.plot(*start, 'kx', markersize=10)
+    plt.plot(*goal, 'kx', markersize=10)
+relaxation = True
+gcs = LinearGCS(regions, edges)
+gcs.addSourceTarget(start, goal)
+gcs.setSolver(MosekSolver())
+waypoints, results_dict = gcs.SolvePath(relaxation)[0]
+
+vertices = gcs.Vertices()
+
+
